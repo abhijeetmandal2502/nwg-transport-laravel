86b1a5939a5ea9a45470102e2ac3c841ee74f7db
Honey Sarkar <69102064+mssoo7@users.noreply.github.com>
2022-02-21 18:31:38 +0530

    some bugs fix in bilty api

--- app/Http/Controllers/api/BiltyController.php
+++ app/Http/Controllers/api/BiltyController.php
@@ -4,24 +4,47 @@ namespace App\Http\Controllers\api;
 
 use App\Http\Controllers\Controller;
 use App\Models\Bilty;
+use App\Models\LRBooking;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 
 class BiltyController extends Controller
 {
 
+    public function getConsignor($lrNo)
+    {
+        $consignor = LRBooking::select('consignor_id')->where('booking_id', $lrNo)->get()->toArray();
+
+        if (!empty($consignor)) {
+
+            return $consignor[0]['consignor_id'];
+        } else {
+            return null;
+        }
+    }
+
     public function createBilty(Request $request)
     {
 
+        $consignorId =  $this->getConsignor($request->booking_id);
+        if ($consignorId == null) {
+            return response(['status' => 'error', 'errors' => 'Consignor not found on this booking!'], 422);
+        }
+        $invoiceUnique = $consignorId . '-' . $request->invoice_no;
+        $request->merge(['invoice' => $invoiceUnique]);
+
+
         $validator = Validator::make($request->all(), [
-            'booking_id' => 'required|alpha_num',
+            'invoice' => 'required|unique:bilties,invoice',
+            'booking_id' => 'required|alpha_num|exists:l_r_bookings,booking_id',
             'shipment_no' => 'required|max:50',
-            'packages' => 'required|numeric',
             'invoice_no' => 'required|max:50',
+            'packages' => 'required|numeric',
+            'description' => 'required|max:150',
             'date' => 'required|date',
             'gst_no' => 'required',
             'goods_value' => 'required|numeric',
-            'created_by' => 'required'
+            'created_by' => 'required|exists:users,emp_id'
         ]);
 
         if ($validator->fails()) {
@@ -31,7 +54,7 @@ class BiltyController extends Controller
         if ($createBilty) {
             return response(['status' => 'success', 'message' => 'Bilty Created successfully!', 'data' => $request->all()], 201);
         } else {
-            return response(['status' => 'error', 'errors' => 'Something went wrong'], 422);
+            return response(['status' => 'error', 'errors' => 'Something went wrong!'], 422);
         }
     }
 }
--- database/migrations/2022_02_21_104740_create_bilties_table.php
+++ database/migrations/2022_02_21_104740_create_bilties_table.php
@@ -15,11 +15,13 @@ class CreateBiltiesTable extends Migration
     {
         Schema::create('bilties', function (Blueprint $table) {
             $table->id();
+            $table->string('invoice')->comment('Unique invoice number with consignor');
             $table->string('booking_id')->comment('lr booking number');
             $table->string('shipment_no')->comment('shipment number');
             $table->integer('packages')->default(0)->comment('No of packages');
             $table->string('invoice_no')->comment('Invoice number');
             $table->dateTime('date')->comment('genrated date');
+            $table->text('description')->comment('description of package');
             $table->string('gst_no')->nullable()->comment('shipment number');
             $table->decimal('goods_value', 10, 2)->default(0)->comment('Package value in amount');
             $table->string('created_by')->nullable()->comment('who generated');
